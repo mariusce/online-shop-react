ceb50a65edbe523b790edc1e0511708166832347
Marius Suta <marius.suta.ce@gmail.com>
2020-02-06 11:53:35 +0200

    Updates

---
+++ precache-manifest.79a624fb6049e714d56cbf20be3aa548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe50163090b94495bf8a91dc258b0340",
+    "url": "/online-shop-react/index.html"
+  },
+  {
+    "revision": "fd670dd5184c9d58ff47",
+    "url": "/online-shop-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc185b7643f626bb5dbc",
+    "url": "/online-shop-react/static/js/2.9a917579.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/online-shop-react/static/js/2.9a917579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd670dd5184c9d58ff47",
+    "url": "/online-shop-react/static/js/main.a4edaf2e.chunk.js"
+  },
+  {
+    "revision": "bad4841ac1c252d5bf00",
+    "url": "/online-shop-react/static/js/runtime-main.03cdd17d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/online-shop-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
