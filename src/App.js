4d8f2879bd551078c71fdd4d00c709aabeab2472
Marius Suta <marius.suta.ce@gmail.com>
2020-02-07 12:01:28 +0200

    added sign in component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/singn-in-and-sign-up/singn-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <SignInAndSignUpPage path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/singn-in-and-sign-up/singn-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './singn-in-and-sign-up.styles.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const SignInAndSignUpPage = () => (
+    <div className="singn-in-and-sign-up">
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
