b48571a55fc2d483419b026e32b4624d0dc3b388
Marius Suta <marius.suta.ce@gmail.com>
2020-02-07 17:25:35 +0200

    added sign in and sign up functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config ={
     appId: "1:469717606200:web:191766bdc22c8ec4db6bcd"
   };
 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user: ', error);
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
